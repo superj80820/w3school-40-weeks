af87fcfe67e38dba95065e48f01d39c3d1e36074
superj80820 <superj80820@gmail.com>
2020-06-21 20:58:08 +0800

    Update week18 port

--- week18/index.js
+++ week18/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+
 app.use(express.static('static'))
 
-app.listen('8080', () => console.log('Server run on port 8080'))
+app.listen('80', () => console.log('Server run on port 80'))
