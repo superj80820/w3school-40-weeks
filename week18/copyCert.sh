ef89c83411bd97b7f9c25372b72340c76cd5d828
superj80820 <superj80820@gmail.com>
2020-06-21 21:49:02 +0800

    Update bash

--- week18/copyCert.sh
+++ week18/copyCert.sh
@@ -3,4 +3,5 @@
 DOMAIN=$1
 cp /etc/letsencrypt/live/$DOMAIN/privkey.pem privkey.pem
 cp /etc/letsencrypt/live/$DOMAIN/cert.pem cert.pem
+cp /etc/letsencrypt/live/$DOMAIN/fullchain.pem fullchain.pem
 chmod 777 ./privkey.pem
