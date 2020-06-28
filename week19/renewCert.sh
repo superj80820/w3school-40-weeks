8e5c1211fdb1e3a87de49a667f36a09dee6aa5cc
superj80820 <superj80820@gmail.com>
2020-06-28 18:37:58 +0800

    Update script

--- week19/renewCert.sh
+++ week19/renewCert.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
-ARN=$1
-aws acm import-certificate --certificate file://cert.pem --certificate-chain file://fullchain.pem --private-key file://privkey.pem --certificate-arn $ARN
+DOMAIN=$1
+FOLDER=$2
+ARN=$3
+aws acm import-certificate --certificate file://$FOLDER/cert.pem --certificate-chain file://$FOLDER/fullchain.pem --private-key file://$FOLDER/privkey.pem --certificate-arn $ARN
