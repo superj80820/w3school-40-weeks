13ff22034026cbb94b6869d5299840be856d5fe0
superj80820 <superj80820@gmail.com>
2020-06-28 18:25:03 +0800

    Update week19 script

--- week19/renewCert.sh
+++ week19/renewCert.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
 DOMAIN=$1
+ARN=$2
 cp /etc/letsencrypt/live/$DOMAIN/privkey.pem privkey.pem
 cp /etc/letsencrypt/live/$DOMAIN/cert.pem cert.pem
 cp /etc/letsencrypt/live/$DOMAIN/fullchain.pem fullchain.pem
 chmod 777 ./privkey.pem
 
-aws acm import-certificate --certificate file://cert.pem --certificate-chain file://fullchain.pem --private-key file://privkey.pem
+aws acm import-certificate --certificate file://cert.pem --certificate-chain file://fullchain.pem --private-key file://privkey.pem --certificate-arn $ARN
